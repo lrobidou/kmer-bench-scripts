5af9e8a43040b82b8c7a384040a735e57631782f
Lucas Robidou <lucas.robidou@uni-bielefeld.de>
2024-09-16 10:50:21 +0200

    add scripts to generate python constants

---
+++ analyse/generate_constants.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TSV_FOLDER=$1
+
+FILES=$(find $TSV_FOLDER -name "*.tsv" -exec basename {} .tsv \;)
+
+for FILE in $FILES; do
+  echo "${FILE} = \"${TSV_FOLDER}/${FILE}.tsv\""
+done
+
+echo "k = 31"
+echo "alpha = 0.4"
+
+mkdir -p graphs/mustaches/kmers/
+mkdir -p graphs/mustaches/reads/
+# sshash = "SSHash/query/query_fusion/query_fusion.tsv"
+# ggcat = "ggcat/query_fusion.tsv"
+# needle = "needle/query_fusion.tsv"
+# reindeer = "REINDEER/query_fusion.tsv"
+# kmindex = "kmindex/query_fusion_19G.tsv"
+# bqf = "bqf/query_results/query_results_fusion_18G.tsv"
+# squeakr = "squeakr/query_fusion/query.tsv"
+# 
+# 
+# 
+# k = 31
+# alpha = 0.4
